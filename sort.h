3da957d60f993e51ab67ae73110005a662762d14
SimoLogiC <mohamedelheimer0@gmail.com>
2023-11-19 20:29:11 +0100

    0-bubble_sort.c

--- sort.h
+++ sort.h
@@ -26,17 +26,17 @@ typedef struct listint_s
  */
 void print_array(const int *array, size_t size)
 {
-    size_t i;
+	size_t i;
 
-    i = 0;
-    while (array && i < size)
-    {
-        if (i > 0)
-            printf(", ");
-        printf("%d", array[i]);
-        ++i;
-    }
-    printf("\n");
+	i = 0;
+	while (array && i < size)
+	{
+		if (i > 0)
+			printf(", ");
+		printf("%d", array[i]);
+		++i;
+	}
+	printf("\n");
 }
 
 /**
@@ -46,18 +46,18 @@ void print_array(const int *array, size_t size)
  */
 void print_list(const listint_t *list)
 {
-    int i;
+	int i;
 
-    i = 0;
-    while (list)
-    {
-        if (i > 0)
-            printf(", ");
-        printf("%d", list->n);
-        ++i;
-        list = list->next;
-    }
-    printf("\n");
+	i = 0;
+	while (list)
+	{
+		if (i > 0)
+			printf(", ");
+		printf("%d", list->n);
+		++i;
+		list = list->next;
+	}
+	printf("\n");
 }
 void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
